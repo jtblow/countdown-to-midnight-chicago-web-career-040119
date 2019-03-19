9e41190ce9de52dfeaf0c0a06b06e3a0c1541f73
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-19 19:12:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,6 +5,15 @@ def countdown(counter)
   while counter >= 2
   counter -= 1 
   puts "#{counter} SECOND(S)!"
+end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(counter)
+  counter = 11 
+  while counter >= 2
+  counter -= 1 
+  puts "#{counter} SECOND(S)!"
   sleep 1 
 end
 "HAPPY NEW YEAR!"
